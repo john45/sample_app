b49902dde27bf0877e6ec31a128abdd01832428c
Evgen Adamenko <mangamen45@gmail.com>
2016-01-03 22:29:39 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ SampleApp::Application.routes.draw do
 
 
   root  'static_pages#home'
+  resources :users
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
-  match '/signup',  to: 'users_controller#new', via: 'get'
+  match '/signup',  to: 'users#new', via: 'get'
 
   # get 'static_pages/help'
 
